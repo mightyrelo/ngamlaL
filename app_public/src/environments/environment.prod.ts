44e5f059c464dacf4c586d72a4eabc7f6975dcfc
katlego <katlego@accsight.org>
2023-03-22 17:46:54 +0200

    done with frontend of product

--- app_public/src/environments/environment.prod.ts
+++ app_public/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
-  apiBaseUrl: 'http://localhost:3000/api',
+  //apiBaseUrl: 'http://localhost:3000/api',
+  apiBaseUrl: 'http://katlego.net/api',
   production: true
 };
